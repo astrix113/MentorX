89ab53aa996cc756833c4deeeeecb9cc16d518e6
khan1095 <ykacn5133@gmail.com>
2025-04-29 09:39:38 +0530

    creating onboarding form.

--- app/(main)/dashboard/page.jsx
+++ app/(main)/dashboard/page.jsx
@@ -1,6 +1,13 @@
+import { getUserOnboardingStatus } from '@/actions/user';
+import { redirect } from 'next/navigation';
 import React from 'react'
 
-const IndustryInsightsPage = () => {
+const IndustryInsightsPage = async() => {
+  const { isOnboarded } = await getUserOnboardingStatus();
+    if (!isOnboarded) {
+      // Redirect to the dashboard or another page
+      redirect('/onboarding')
+    }
   return (
     <div>IndustryInsightsPage</div>
   )
--- app/(main)/onboarding/page.jsx
+++ app/(main)/onboarding/page.jsx
@@ -1,9 +1,15 @@
 import { industries } from '@/data/industries'
 import React from 'react'
 import OnboardingForm from './_components/onboarding-form'
+import { getUserOnboardingStatus } from '@/actions/user'
 
-const OnboardingPage = () => {
+const OnboardingPage = async() => {
   //check if user is alredy onboarded
+  const { isOnboarded } = await getUserOnboardingStatus();
+  if (isOnboarded) {
+    // Redirect to the dashboard or another page
+    redirect('/dashboard')
+  }
   return (
     <main>
       <OnboardingForm industries={industries} />
